34d522be4fb385c83fdbfff3b8825a048c766de1
stephan-v <stephan-v@hotmail.com>
2018-09-05 00:16:42 +0200

    Added seeders for the server and project

---
+++ database/seeds/ProjectsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('projects')->insert([
+            'name' => 'BeerQuest',
+            'repository' => 'stephan-v/beerquest',
+            'user_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
---
+++ database/seeds/ServersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+
+class ServersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('servers')->insert([
+            'name' => 'BeerQuest',
+            'path' => '/home/forge/downstream-test.nl',
+            'ip_address' => '188.166.100.191',
+            'user' => 'forge',
+            'project_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
