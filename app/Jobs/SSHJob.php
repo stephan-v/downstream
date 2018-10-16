7035219ce10ffd60068ad8aa602c8af3ea21e3b7
stephan-v <stephan-v@hotmail.com>
2018-10-16 22:15:16 +0200

    Removed the AbstractJob since everything has been refactored to a single SSHJob anyways

--- app/Jobs/SSHJob.php
+++ app/Jobs/SSHJob.php
@@ -4,7 +4,10 @@ namespace App\Jobs;
 
 use App\Action;
 use App\Deployment;
-use App\Ssh\AbstractJob;
+use App\Events\DeploymentFinished;
+use App\Job;
+use App\Server;
+use App\Ssh\CompilesCommandsTrait;
 use App\Ssh\SSH;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
@@ -12,9 +15,37 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class SSHJob extends AbstractJob implements ShouldQueue
+class SSHJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, CompilesCommandsTrait;
+
+    /**
+     * The bash commands that are associated with this job.
+     *
+     * @var array $commands Array of commands to run one by one on a specified server.
+     */
+    private $commands;
+
+    /**
+     * The freshly started deployment.
+     *
+     * @var Deployment $deployment
+     */
+    private $deployment;
+
+    /**
+     * The name of the current job.
+     *
+     * @var string $name
+     */
+    private $name;
+
+    /**
+     * The created jobs.
+     *
+     * @var Job[] $jobs
+     */
+    private $jobs;
 
     /**
      * CloneRepository constructor.
@@ -24,12 +55,42 @@ class SSHJob extends AbstractJob implements ShouldQueue
      */
     public function __construct(Deployment $deployment, Action $action)
     {
-        $name = $action->name;
-        $commands = unserialize($action->script);
+        $this->deployment = $deployment;
+        $this->name = $action->name;
+        $this->commands = unserialize($action->script);
 
-        parent::__construct($deployment, $name, $commands);
+        $this->saveJobsToDatabase();
     }
 
+    /**
+     * Save the jobs to the database.
+     */
+    protected function saveJobsToDatabase()
+    {
+        $servers = $this->deployment->project->servers;
+
+        foreach ($servers as $server) {
+            $this->createJob($server);
+        }
+    }
+
+    /**
+     * Persist job to the database.
+     *
+     * @param Server $server
+     */
+    private function createJob(Server $server)
+    {
+        $job = new Job();
+
+        $job->name = $this->name;
+        $job->commands = $this->compileCommands($server);
+        $job->deployment()->associate($this->deployment);
+        $job->server()->associate($server);
+        $job->status = Job::PENDING;
+
+        $this->jobs[] = tap($job)->save();
+    }
 
     /**
      * Execute the job.
@@ -42,5 +103,19 @@ class SSHJob extends AbstractJob implements ShouldQueue
             $ssh->setJob($job);
             $ssh->fire();
         }
+    }
+
+    /**
+     * The job failed to process.
+     *
+     * If a job failed in the chain we make sure to set the entire deployment to 'failed'.
+     */
+    public function failed()
+    {
+        $deployment = $this->deployment;
+        $deployment->status = Deployment::FAILED;
+        $deployment->save();
+
+        event(new DeploymentFinished($deployment, Deployment::FAILED));
     }
 }
